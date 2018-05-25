633dfb25ea64dcf652e2dd590ddadc0504643e4a
hrodriguez <hrodriguez@DESKTOP-CR6NB3K>
2018-05-25 14:19:39 -0500

    DCDC-48: Research / Pruebas con plugin mylyn
    Task-Url: http://jira.consultec-ti.com/browse/DCDC-48
    
    This is a kind of test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application designed to perform basic operations on a postgres database.
+ * In addition, it has some JPA annotations.
+ * @author hrodriguez
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
 
